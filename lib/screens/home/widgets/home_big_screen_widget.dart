5fe344fc337744e5c3881c6a13e9714b657ae357
Max <max.chiang@kkday.com>
2023-03-31 20:30:00 +0800

    week1 refactor code

--- lib/screens/home/widgets/home_big_screen_widget.dart
+++ lib/screens/home/widgets/home_big_screen_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:stylish_max/models/product_category.dart';
 import 'package:stylish_max/screens/home/component/home_page_list_title.dart';
-import 'package:stylish_max/screens/home/widgets/home_big_screen_page_list.dart';
 import 'package:stylish_max/screens/home/widgets/home_page_banner.dart';
+import 'package:stylish_max/screens/home/widgets/home_page_list_item.dart';
 
 class HomeBigScreenWidget extends StatelessWidget {
   const HomeBigScreenWidget({super.key, required this.productCategoryList});
@@ -50,5 +50,35 @@ class HomeBigScreenWidget extends StatelessWidget {
         ),
       )
     ]);
+  }
+}
+
+class HomeBigScreenPageList extends StatefulWidget {
+  const HomeBigScreenPageList({
+    super.key,
+    required this.productCategory,
+  });
+
+  final ProductCategory productCategory;
+
+  @override
+  State<HomeBigScreenPageList> createState() => _HomeBigScreenPageListState();
+}
+
+class _HomeBigScreenPageListState extends State<HomeBigScreenPageList>
+    with AutomaticKeepAliveClientMixin {
+  bool isExpanded = true;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Column(
+      children: widget.productCategory.productList
+          .map((product) => HomePageListItem(product: product))
+          .toList(),
+    );
   }
 }
--- lib/screens/home/widgets/home_small_screen_widget.dart
+++ lib/screens/home/widgets/home_small_screen_widget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:stylish_max/models/product_category.dart';
+import 'package:stylish_max/screens/home/component/home_page_list_title.dart';
 import 'package:stylish_max/screens/home/widgets/home_page_banner.dart';
-import 'package:stylish_max/screens/home/widgets/home_small_screen_page_list.dart';
+import 'package:stylish_max/screens/home/widgets/home_page_list_item.dart';
 
 class HomeSmallScreenWidget extends StatelessWidget {
   const HomeSmallScreenWidget({super.key, required this.productCategoryList});
@@ -22,6 +23,51 @@ class HomeSmallScreenWidget extends StatelessWidget {
           productCategory: productCategoryList[index - 1],
         );
       },
+    );
+  }
+}
+
+class HomeSmallScreenPageList extends StatefulWidget {
+  const HomeSmallScreenPageList({
+    super.key,
+    required this.productCategory,
+  });
+
+  final ProductCategory productCategory;
+
+  @override
+  State<HomeSmallScreenPageList> createState() =>
+      _HomeSmallScreenPageListState();
+}
+
+class _HomeSmallScreenPageListState extends State<HomeSmallScreenPageList>
+    with AutomaticKeepAliveClientMixin {
+  bool isExpanded = true;
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+          child: HomePageListTitle(title: widget.productCategory.title),
+        ),
+        isExpanded
+            ? Column(
+                children: widget.productCategory.productList
+                    .map((product) => HomePageListItem(product: product))
+                    .toList(),
+              )
+            : Container(),
+      ],
     );
   }
 }
